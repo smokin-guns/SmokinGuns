b2786567f70a56e8f1cb081523560be2202610b5
sg_tequila <sg_tequila@07eb42f0-b032-4355-afd3-42b1f3832223>
2009-02-06 19:25:50 +0000

    Merge nextmap init fix from rev 130 trunk by Joe Kari
    git-svn-id: https://smokinguns.svn.sourceforge.net/svnroot/smokinguns/branches/1.1@134 07eb42f0-b032-4355-afd3-42b1f3832223

--- code/server/sv_init.c
+++ code/server/sv_init.c
@@ -456,8 +456,12 @@ void SV_SpawnServer( char *server, qboolean killBots ) {
 
 	// set nextmap to the same map, but it may be overriden
 	// by the game startup or another console command
+	
+	// Joe Kari: this is what cause the 'nextmap' to bug, I remove that if SMOKINGUNS is defined
+#ifndef SMOKINGUNS
 	Cvar_Set( "nextmap", "map_restart 0");
 //	Cvar_Set( "nextmap", va("map %s", server) );
+#endif
 
 	for (i=0 ; i<sv_maxclients->integer ; i++) {
 		// save when the server started for each client already connected
