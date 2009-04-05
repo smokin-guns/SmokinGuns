b73a85f1c94a8060ebb1cd68d91b40dcd0b027bb
sg_tequila <sg_tequila@07eb42f0-b032-4355-afd3-42b1f3832223>
2009-04-05 12:40:05 +0000

    Forgotten file at the previous commit
    git-svn-id: http://svn.code.sf.net/p/smokinguns/code/branches/1.1@165 07eb42f0-b032-4355-afd3-42b1f3832223

--- code/game/ai_cmd.h
+++ code/game/ai_cmd.h
@@ -21,6 +21,7 @@ along with Smokin' Guns; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 ===========================================================================
 */
+//
 
 /*****************************************************************************
  * name:		ai_cmd.h
