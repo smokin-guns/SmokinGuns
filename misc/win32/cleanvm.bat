754def61741b3a9f79f9f7675aad71cb3f4a5e6e
sparcdr <sparcdr@07eb42f0-b032-4355-afd3-42b1f3832223>
2009-04-10 19:46:22 +0000

    Build successfully needs work on QVM build process.  Generated binary throws error about invalid checksum due to QVM.  Added Windows batch files for QVM build/clean but not finished.  All redundant/untested profiles in solutions are purged.
    git-svn-id: https://smokinguns.svn.sourceforge.net/svnroot/smokinguns/branches/1.1@180 07eb42f0-b032-4355-afd3-42b1f3832223

---
+++ misc/win32/cleanvm.bat
@@ -0,0 +1,10 @@
+@echo "Cleaning ASM files ..."
+@rd /S /Q ..\..\build\asm
+
+@echo "Cleaning MAP files ..."
+@rd /S /Q ..\..\build\map
+
+@echo "Cleaning VM files ..."
+@rd /S /Q ..\..\build\vm
+
+pause
