62b59701f50a1b65e8d8c5d3c5794f5551a50e85
sg_tequila <sg_tequila@07eb42f0-b032-4355-afd3-42b1f3832223>
2009-04-11 07:31:06 +0000

    Synchronised batch file with the unix Makefile way of building QVM Feel free to fix, I can test it (very sorry :P)
    git-svn-id: https://smokinguns.svn.sourceforge.net/svnroot/smokinguns@184 07eb42f0-b032-4355-afd3-42b1f3832223

--- branches/1.1/misc/win32/buildvm.bat
+++ branches/1.1/misc/win32/buildvm.bat
@@ -5,129 +5,126 @@
 
 mkdir ..\..\build
 mkdir ..\..\build\asm
-mkdir ..\..\build\map
 mkdir ..\..\build\vm
 cd ..\..\build\asm
 
 @set PATH_GAME=..\..\code\game
-@set PATH_CGAME=..\code\cgame
-@set PATH_UI=..\code\ui
+@set PATH_CGAME=..\..\code\cgame
+@set PATH_UI=..\..\code\ui
 
-@set CC=lcc -DQ3_VM -DMISSIONPACK -DSMOKINGUNS -S -Wf-target=bytecode -Wf-g -I%PATH_CGAME% -I%PATH_GAME% -I%PATH_UI% %1
-@set CC_CGAME=lcc -DQ3_VM -DVM_CGAME -DMISSIONPACK -S -DSMOKINGUNS -Wf-target=bytecode -Wf-g -I%PATH_CGAME% -I%PATH_GAME% -I%PATH_UI% %1
+@set CC_GAME=q3lcc -DSMOKINGUNS -DQAGAME %1
+@set CC_CGAME=q3lcc -DSMOKINGUNS -DCGAME %1
+@set CC_UI=q3lcc -DSMOKINGUNS -DUI %1
 @set Q3ASM=q3asm
 
 @echo Building game ...
 
-@if not exist g_main.asm %CC% %PATH_GAME%/g_main.c
+@if not exist g_main.asm %CC_GAME% %PATH_GAME%/g_main.c
 @if errorlevel 1 goto quit
 
-@if not exist bg_misc.asm %CC% %PATH_GAME%/bg_misc.c
+@if not exist bg_misc.asm %CC_GAME% %PATH_GAME%/bg_misc.c
 @if errorlevel 1 goto quit
 
-@if not exist bg_lib.asm %CC% %PATH_GAME%/bg_lib.c
+@if not exist bg_lib.asm %CC_GAME% %PATH_GAME%/bg_lib.c
 @if errorlevel 1 goto quit
 
-@if not exist bg_pmove.asm %CC% %PATH_GAME%/bg_pmove.c
+@if not exist bg_pmove.asm %CC_GAME% %PATH_GAME%/bg_pmove.c
 @if errorlevel 1 goto quit
 
-@if not exist bg_slidemove.asm %CC% %PATH_GAME%/bg_slidemove.c
+@if not exist bg_slidemove.asm %CC_GAME% %PATH_GAME%/bg_slidemove.c
 @if errorlevel 1 goto quit
 
-@if not exist q_math.asm %CC% %PATH_GAME%/q_math.c
+@if not exist q_math.asm %CC_GAME% %PATH_GAME%/q_math.c
 @if errorlevel 1 goto quit
 
-@if not exist q_shared.asm %CC% %PATH_GAME%/q_shared.c
+@if not exist q_shared.asm %CC_GAME% %PATH_GAME%/q_shared.c
 @if errorlevel 1 goto quit
 
-@if not exist ai_dmnet.asm %CC% %PATH_GAME%/ai_dmnet.c
+@if not exist ai_dmnet.asm %CC_GAME% %PATH_GAME%/ai_dmnet.c
 @if errorlevel 1 goto quit
 
-@if not exist ai_dmq3.asm %CC% %PATH_GAME%/ai_dmq3.c
+@if not exist ai_dmq3.asm %CC_GAME% %PATH_GAME%/ai_dmq3.c
 @if errorlevel 1 goto quit
 
-@if not exist ai_main.asm %CC% %PATH_GAME%/ai_main.c
+@if not exist ai_main.asm %CC_GAME% %PATH_GAME%/ai_main.c
 @if errorlevel 1 goto quit
 
-@if not exist ai_chat.asm %CC% %PATH_GAME%/ai_chat.c
+@if not exist ai_chat.asm %CC_GAME% %PATH_GAME%/ai_chat.c
 @if errorlevel 1 goto quit
 
-@if not exist ai_cmd.asm %CC% %PATH_GAME%/ai_cmd.c
+@if not exist ai_cmd.asm %CC_GAME% %PATH_GAME%/ai_cmd.c
 @if errorlevel 1 goto quit
 
-@if not exist ai_team.asm %CC% %PATH_GAME%/ai_team.c
+@if not exist ai_team.asm %CC_GAME% %PATH_GAME%/ai_team.c
 @if errorlevel 1 goto quit
 
-@if not exist g_active.asm %CC% %PATH_GAME%/g_active.c
+@if not exist g_active.asm %CC_GAME% %PATH_GAME%/g_active.c
 @if errorlevel 1 goto quit
 
-@if not exist g_arenas.asm %CC% %PATH_GAME%/g_arenas.c
+@if not exist g_arenas.asm %CC_GAME% %PATH_GAME%/g_arenas.c
 @if errorlevel 1 goto quit
 
-@if not exist g_bot.asm %CC% %PATH_GAME%/g_bot.c
+@if not exist g_bot.asm %CC_GAME% %PATH_GAME%/g_bot.c
 @if errorlevel 1 goto quit
 
-@if not exist g_client.asm %CC% %PATH_GAME%/g_client.c
+@if not exist g_client.asm %CC_GAME% %PATH_GAME%/g_client.c
 @if errorlevel 1 goto quit
 
-@if not exist g_cmds.asm %CC% %PATH_GAME%/g_cmds.c
+@if not exist g_cmds.asm %CC_GAME% %PATH_GAME%/g_cmds.c
 @if errorlevel 1 goto quit
 
-@if not exist g_combat.asm %CC% %PATH_GAME%/g_combat.c
+@if not exist g_combat.asm %CC_GAME% %PATH_GAME%/g_combat.c
 @if errorlevel 1 goto quit
 
-@if not exist g_items.asm %CC% %PATH_GAME%/g_items.c
+@if not exist g_items.asm %CC_GAME% %PATH_GAME%/g_items.c
 @if errorlevel 1 goto quit
 
-@if not exist g_mem.asm %CC% %PATH_GAME%/g_mem.c
+@if not exist g_mem.asm %CC_GAME% %PATH_GAME%/g_mem.c
 @if errorlevel 1 goto quit
 
-@if not exist g_misc.asm %CC% %PATH_GAME%/g_misc.c
+@if not exist g_misc.asm %CC_GAME% %PATH_GAME%/g_misc.c
 @if errorlevel 1 goto quit
 
-@if not exist g_missile.asm %CC% %PATH_GAME%/g_missile.c
+@if not exist g_missile.asm %CC_GAME% %PATH_GAME%/g_missile.c
 @if errorlevel 1 goto quit
 
-@if not exist g_mover.asm %CC% %PATH_GAME%/g_mover.c
+@if not exist g_mover.asm %CC_GAME% %PATH_GAME%/g_mover.c
 @if errorlevel 1 goto quit
 
-@if not exist g_session.asm %CC% %PATH_GAME%/g_session.c
+@if not exist g_session.asm %CC_GAME% %PATH_GAME%/g_session.c
 @if errorlevel 1 goto quit
 
-@if not exist g_spawn.asm %CC% %PATH_GAME%/g_spawn.c
+@if not exist g_spawn.asm %CC_GAME% %PATH_GAME%/g_spawn.c
 @if errorlevel 1 goto quit
 
-@if not exist g_svcmds.asm %CC% %PATH_GAME%/g_svcmds.c
+@if not exist g_svcmds.asm %CC_GAME% %PATH_GAME%/g_svcmds.c
 @if errorlevel 1 goto quit
 
-@if not exist g_target.asm %CC% %PATH_GAME%/g_target.c
+@if not exist g_target.asm %CC_GAME% %PATH_GAME%/g_target.c
 @if errorlevel 1 goto quit
 
-@if not exist g_team.asm %CC% %PATH_GAME%/g_team.c
+@if not exist g_team.asm %CC_GAME% %PATH_GAME%/g_team.c
 @if errorlevel 1 goto quit
 
-@if not exist g_trigger.asm %CC% %PATH_GAME%/g_trigger.c
+@if not exist g_trigger.asm %CC_GAME% %PATH_GAME%/g_trigger.c
 @if errorlevel 1 goto quit
 
-@if not exist g_utils.asm %CC% %PATH_GAME%/g_utils.c
+@if not exist g_utils.asm %CC_GAME% %PATH_GAME%/g_utils.c
 @if errorlevel 1 goto quit
 
-@if not exist g_weapon.asm %CC% %PATH_GAME%/g_weapon.c
+@if not exist g_weapon.asm %CC_GAME% %PATH_GAME%/g_weapon.c
 @if errorlevel 1 goto quit
 
-@if not exist ai_vcmd.asm %CC% %PATH_GAME%/ai_vcmd.c
+@if not exist ai_vcmd.asm %CC_GAME% %PATH_GAME%/ai_vcmd.c
 @if errorlevel 1 goto quit
 
-@if not exist g_unlagged.asm %CC% %PATH_GAME%/g_unlagged.c
+@if not exist g_unlagged.asm %CC_GAME% %PATH_GAME%/g_unlagged.c
 @if errorlevel 1 goto quit
 
-@if not exist g_sg_utils.asm %CC% %PATH_GAME%/g_sg_utils.c
+@if not exist g_sg_utils.asm %CC_GAME% %PATH_GAME%/g_sg_utils.c
 @if errorlevel 1 goto quit
 
-@if not exist g_hit.asm %CC% %PATH_GAME%/g_hit.c
-@if errorlevel 1 goto quit
-
-@if not exist g_unlagged.asm %CC% %PATH_GAME%/g_unlagged.c
+@if not exist g_hit.asm %CC_GAME% %PATH_GAME%/g_hit.c
 @if errorlevel 1 goto quit
 
 @echo Building cgame ...
@@ -207,7 +204,7 @@ cd ..\..\build\asm
 @if not exist ui_shared.asm %CC_CGAME% %PATH_UI%/ui_shared.c
 @if errorlevel 1 goto quit
 
-@if not exist cg_newDraw.asm %CC_CGAME% %PATH_CGAME%/cg_newDraw.c
+@if not exist cg_newDraw.asm %CC_CGAME% %PATH_CGAME%/cg_newdraw.c
 @if errorlevel 1 goto quit
 
 @if not exist cg_sg_utils.asm %CC_CGAME% %PATH_CGAME%/cg_sg_utils.c
@@ -218,34 +215,31 @@ cd ..\..\build\asm
 
 @echo Building ui ...
 
-@if not exist ui_main.asm %CC% %PATH_UI%/ui_main.c
+@if not exist ui_main.asm %CC_UI% %PATH_UI%/ui_main.c
 @if errorlevel 1 goto quit
 
-@if not exist bg_misc.asm %CC% %PATH_GAME%/bg_misc.c
+@if not exist bg_misc.asm %CC_UI% %PATH_GAME%/bg_misc.c
 @if errorlevel 1 goto quit
 
-@if not exist bg_lib.asm %CC% %PATH_GAME%/bg_lib.c
+@if not exist bg_lib.asm %CC_UI% %PATH_GAME%/bg_lib.c
 @if errorlevel 1 goto quit
 
-@if not exist q_math.asm %CC% %PATH_GAME%/q_math.c
+@if not exist q_math.asm %CC_UI% %PATH_GAME%/q_math.c
 @if errorlevel 1 goto quit
 
-@if not exist q_shared.asm %CC% %PATH_GAME%/q_shared.c
+@if not exist q_shared.asm %CC_UI% %PATH_GAME%/q_shared.c
 @if errorlevel 1 goto quit
 
-@if not exist ui_atoms.asm %CC% %PATH_UI%/ui_atoms.c
+@if not exist ui_atoms.asm %CC_UI% %PATH_UI%/ui_atoms.c
 @if errorlevel 1 goto quit
 
-@if not exist ui_players.asm %CC% %PATH_UI%/ui_players.c
+@if not exist ui_players.asm %CC_UI% %PATH_UI%/ui_players.c
 @if errorlevel 1 goto quit
 
-@if not exist ui_util.asm %CC% %PATH_UI%/ui_util.c
+@if not exist ui_shared.asm %CC_UI% %PATH_UI%/ui_shared.c
 @if errorlevel 1 goto quit
 
-@if not exist ui_shared.asm %CC% %PATH_UI%/ui_shared.c
-@if errorlevel 1 goto quit
-
-@if not exist ui_gameinfo.asm %CC% %PATH_UI%/ui_gameinfo.c
+@if not exist ui_gameinfo.asm %CC_UI% %PATH_UI%/ui_gameinfo.c
 @if errorlevel 1 goto quit
 
 @set Q3ASM_ARGS=-o qagame
@@ -288,7 +282,6 @@ cd ..\..\build\asm
 @set Q3ASM_ARGS=%Q3ASM_ARGS% g_hit
 @if not exist ..\vm\qagame.qvm %Q3ASM% %Q3ASM_ARGS%
 @if exist qagame.qvm move qagame.qvm ..\vm
-@if exist qagame.map move qagame.map ..\map
 
 @set Q3ASM_ARGS=-o cgame
 @set Q3ASM_ARGS=%Q3ASM_ARGS% cg_main
@@ -317,19 +310,17 @@ cd ..\..\build\asm
 @set Q3ASM_ARGS=%Q3ASM_ARGS% q_math
 @set Q3ASM_ARGS=%Q3ASM_ARGS% q_shared
 @set Q3ASM_ARGS=%Q3ASM_ARGS% ui_shared
-@set Q3ASM_ARGS=%Q3ASM_ARGS% cg_newDraw
+@set Q3ASM_ARGS=%Q3ASM_ARGS% cg_newdraw
 @set Q3ASM_ARGS=%Q3ASM_ARGS% cg_sg_utils
 @set Q3ASM_ARGS=%Q3ASM_ARGS% cg_unlagged
 @if not exist ..\vm\cgame.qvm %Q3ASM% %Q3ASM_ARGS%
 @if exist cgame.qvm move cgame.qvm ..\vm
-@if exist cgame.map move cgame.map ..\map
 
 @set Q3ASM_ARGS=-o ui
 @set Q3ASM_ARGS=%Q3ASM_ARGS% ui_main
 @set Q3ASM_ARGS=%Q3ASM_ARGS% %PATH_UI%\ui_syscalls
 @set Q3ASM_ARGS=%Q3ASM_ARGS% ui_atoms
 @set Q3ASM_ARGS=%Q3ASM_ARGS% ui_players
-@set Q3ASM_ARGS=%Q3ASM_ARGS% ui_util
 @set Q3ASM_ARGS=%Q3ASM_ARGS% ui_shared
 @set Q3ASM_ARGS=%Q3ASM_ARGS% ui_gameinfo
 @set Q3ASM_ARGS=%Q3ASM_ARGS% bg_misc
@@ -338,7 +329,6 @@ cd ..\..\build\asm
 @set Q3ASM_ARGS=%Q3ASM_ARGS% q_shared
 @if not exist ..\vm\ui.qvm %Q3ASM% %Q3ASM_ARGS%
 @if exist ui.qvm move ui.qvm ..\vm
-@if exist ui.map move ui.map ..\map
 
 :quit
 pause
