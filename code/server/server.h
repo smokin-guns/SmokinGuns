fb58b8b67103c45220ded38260673bde364253b9
sg_tequila <sg_tequila@07eb42f0-b032-4355-afd3-42b1f3832223>
2009-05-27 08:55:11 +0000

    Update toward ioQuake3 r1558, essentially ip ban API update by Thilo: - Introduce new NET_CompareBaseAdrMask for easy comparison of ip address ranges - Overhaul of the new banning functions:   * basic check for redundant bans/exceptions   * introduction of sv_banFile to make it possible to configure the file where to read bans and exceptions from   * bans can now be deleted by giving address ranges, too.
    git-svn-id: http://svn.code.sf.net/p/smokinguns/code/branches/1.1@228 07eb42f0-b032-4355-afd3-42b1f3832223

--- code/server/server.h
+++ code/server/server.h
@@ -235,7 +235,6 @@ typedef struct {
 } serverStatic_t;
 
 #define SERVER_MAXBANS	1024
-#define SERVER_BANFILE	"serverbans.dat"
 // Structure for managing bans
 typedef struct
 {
@@ -281,6 +280,7 @@ extern	cvar_t	*sv_pure;
 extern	cvar_t	*sv_floodProtect;
 extern	cvar_t	*sv_lanForceRate;
 extern	cvar_t	*sv_strictAuth;
+extern	cvar_t	*sv_banFile;
 
 extern	serverBan_t serverBans[SERVER_MAXBANS];
 extern	int serverBansCount;
