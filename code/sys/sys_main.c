1db0f36b122e3bb49fd644fc46771bf7ec6cafb1
sg_tequila <sg_tequila@07eb42f0-b032-4355-afd3-42b1f3832223>
2009-09-16 18:13:58 +0000

    ioQ3 Synchro to r1631: r1630: Completed revert of rev1626 r1631: Cleanups for the Mac error dialog, after a code review from tma.
    git-svn-id: http://svn.code.sf.net/p/smokinguns/code/branches/1.1@293 07eb42f0-b032-4355-afd3-42b1f3832223

--- code/sys/sys_main.c
+++ code/sys/sys_main.c
@@ -49,7 +49,6 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 static char binaryPath[ MAX_OSPATH ] = { 0 };
 static char installPath[ MAX_OSPATH ] = { 0 };
-static char libPath[ MAX_OSPATH ] = { 0 };
 
 /*
 =================
@@ -94,29 +93,6 @@ char *Sys_DefaultInstallPath(void)
 {
 	if (*installPath)
 		return installPath;
-	else
-		return Sys_Cwd();
-}
-
-/*
-=================
-Sys_SetDefaultLibPath
-=================
-*/
-void Sys_SetDefaultLibPath(const char *path)
-{
-	Q_strncpyz(libPath, path, sizeof(libPath));
-}
-
-/*
-=================
-Sys_DefaultLibPath
-=================
-*/
-char *Sys_DefaultLibPath(void)
-{
-	if (*libPath)
-		return libPath;
 	else
 		return Sys_Cwd();
 }
@@ -416,7 +392,6 @@ Used to load a development dll instead of a virtual machine
 #1 look down current path
 #2 look in fs_homepath
 #3 look in fs_basepath
-#4 look in fs_libpath
 =================
 */
 void *Sys_LoadDll( const char *name, char *fqpath ,
@@ -427,7 +402,6 @@ void *Sys_LoadDll( const char *name, char *fqpath ,
 	void  (*dllEntry)( intptr_t (*syscallptr)(intptr_t, ...) );
 	char  fname[MAX_OSPATH];
 	char  *basepath;
-	char  *libpath;
 	char  *homepath;
 	char  *pwdpath;
 	char  *gamedir;
@@ -439,7 +413,6 @@ void *Sys_LoadDll( const char *name, char *fqpath ,
 	// TODO: use fs_searchpaths from files.c
 	pwdpath = Sys_Cwd();
 	basepath = Cvar_VariableString( "fs_basepath" );
-	libpath = Cvar_VariableString( "fs_libpath" );
 	homepath = Cvar_VariableString( "fs_homepath" );
 	gamedir = Cvar_VariableString( "fs_game" );
 
@@ -450,9 +423,6 @@ void *Sys_LoadDll( const char *name, char *fqpath ,
 
 	if(!libHandle && basepath)
 		libHandle = Sys_TryLibraryLoad(basepath, gamedir, fname, fqpath);
-
-	if(!libHandle && libpath)
-		libHandle = Sys_TryLibraryLoad(libpath, gamedir, fname, fqpath);
 
 	if(!libHandle) {
 		Com_Printf ( "Sys_LoadDll(%s) failed to load library\n", name );
@@ -516,14 +486,6 @@ void Sys_ParseArgs( int argc, char **argv )
 #		define DEFAULT_BASEDIR Sys_StripAppBundle(Sys_BinaryPath())
 #	else
 #		define DEFAULT_BASEDIR Sys_BinaryPath()
-#	endif
-#endif
-
-#ifndef DEFAULT_LIBDIR
-#	ifdef MACOS_X
-#		define DEFAULT_LIBDIR Sys_StripAppBundle(Sys_BinaryPath())
-#	else
-#		define DEFAULT_LIBDIR Sys_BinaryPath()
 #	endif
 #endif
 
@@ -602,7 +564,6 @@ int main( int argc, char **argv )
 	Sys_ParseArgs( argc, argv );
 	Sys_SetBinaryPath( Sys_Dirname( argv[ 0 ] ) );
 	Sys_SetDefaultInstallPath( DEFAULT_BASEDIR );
-	Sys_SetDefaultLibPath( DEFAULT_LIBDIR );
 
 	// Concatenate the command line for passing to Com_Init
 	for( i = 1; i < argc; i++ )
