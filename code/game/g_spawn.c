2ee881ce48798f6db33be9b158a074d277a01599
sg_tequila <sg_tequila@07eb42f0-b032-4355-afd3-42b1f3832223>
2009-09-22 23:53:29 +0000

    Fix "gametype" map entity usage with dedicated SG values
    git-svn-id: http://svn.code.sf.net/p/smokinguns/code/branches/1.1@303 07eb42f0-b032-4355-afd3-42b1f3832223

--- code/game/g_spawn.c
+++ code/game/g_spawn.c
@@ -465,7 +465,11 @@ void G_SpawnGEntityFromSpawnVars( void ) {
 	int			i;
 	gentity_t	*ent;
 	char		*s, *value, *gametypeName;
+#ifndef SMOKINGUNS
 	static char *gametypeNames[] = {"ffa", "tournament", "single", "team", "ctf", "oneflag", "obelisk", "harvester", "teamtournament"};
+#else
+	static char *gametypeNames[] = {"ffa", "duel", "single", "team", "rtp", "br"};
+#endif
 
 	// get the next free entity
 	ent = G_Spawn();
