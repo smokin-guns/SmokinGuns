264a0615cc3985df76d4bf16dc0a8fe39eed5b4e
sg_tequila <sg_tequila@07eb42f0-b032-4355-afd3-42b1f3832223>
2009-10-12 13:17:19 +0000

    ioQ3 Synchro to r1658 with SG_RELEASE bump:  - thilo: fs_fakeChkSum is indeed obsolete and unnecessary. Removing it altogether now.
    git-svn-id: https://smokinguns.svn.sourceforge.net/svnroot/smokinguns@333 07eb42f0-b032-4355-afd3-42b1f3832223

--- branches/1.1/code/client/cl_main.c
+++ branches/1.1/code/client/cl_main.c
@@ -1719,7 +1719,7 @@ CL_ResetPureClientAtServer
 =================
 */
 void CL_ResetPureClientAtServer( void ) {
-	CL_AddReliableCommand(va("vdr"), qfalse);
+	CL_AddReliableCommand("vdr", qfalse);
 }
 
 /*
