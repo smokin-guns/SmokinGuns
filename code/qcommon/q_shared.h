7e94592eb2aac211eefe1445119417e090da7f11
sg_tequila <sg_tequila@07eb42f0-b032-4355-afd3-42b1f3832223>
2009-10-12 13:17:19 +0000

    ioQ3 Synchro to r1658 with SG_RELEASE bump:  - thilo: fs_fakeChkSum is indeed obsolete and unnecessary. Removing it altogether now.
    git-svn-id: https://smokinguns.svn.sourceforge.net/svnroot/smokinguns/branches/1.1@333 07eb42f0-b032-4355-afd3-42b1f3832223

--- code/qcommon/q_shared.h
+++ code/qcommon/q_shared.h
@@ -78,7 +78,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
     #define PRODUCT_VERSION	1.1
   #endif
   #ifndef SG_RELEASE
-    #define SG_RELEASE	20091011
+    #define SG_RELEASE	20091012
   #endif
 
   #ifndef XSTRING
