886ebf25d40cb0a0f1aff7051071baa788cd2619
sg_tequila <sg_tequila@07eb42f0-b032-4355-afd3-42b1f3832223>
2009-11-02 11:15:37 +0000

    Removed UI syscall related to PunkBuster
    git-svn-id: https://smokinguns.svn.sourceforge.net/svnroot/smokinguns/branches/1.1@386 07eb42f0-b032-4355-afd3-42b1f3832223

--- code/client/cl_ui.c
+++ code/client/cl_ui.c
@@ -974,10 +974,10 @@ intptr_t CL_UISystemCalls( intptr_t *args ) {
 	case UI_SET_CDKEY:
 		CLUI_SetCDKey( VMA(1) );
 		return 0;
-#endif
 
 	case UI_SET_PBCLSTATUS:
 		return 0;
+#endif
 
 	case UI_R_REGISTERFONT:
 		re.RegisterFont( VMA(1), args[2], VMA(3));
