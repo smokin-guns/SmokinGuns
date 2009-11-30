46b8bf9b7d7adfa11570a6e4dce3b5f1dffb4648
sg_tequila <sg_tequila@07eb42f0-b032-4355-afd3-42b1f3832223>
2009-11-30 13:38:46 +0000

    Removed g_unlagged and g_url not really used serverinfo cvars Cleanup few spaces at end of lines Introduced PRODUCT_SHORTNAME and PRODUCT_URL Better use old and new shared defined string from q_shared.h Clean tr_local.h from not used definition, fix comment in tr_bsp.c
    git-svn-id: http://svn.code.sf.net/p/smokinguns/code/branches/1.1@419 07eb42f0-b032-4355-afd3-42b1f3832223

--- code/game/inv.h
+++ code/game/inv.h
@@ -23,7 +23,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 #ifndef SMOKINGUNS
-#error "This file can only be used with Smokin' Guns sources"
+#error "This file can only be used with " PRODUCT_NAME " sources"
 #endif
 
 #define INVENTORY_NONE				0
--- code/qcommon/q_shared.h
+++ code/qcommon/q_shared.h
@@ -63,6 +63,8 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
   #endif
 
   #define PRODUCT_NAME				"Smokin' Guns"
+  #define PRODUCT_SHORTNAME			"SmokinGuns"
+  #define PRODUCT_URL				"http://www.smokin-guns.net"
   #define BASEGAME					"smokinguns"
   // Tequila comment: SDK_BASEGAME is used for compatibility reason
   // If we don't use it, 1.0 client won't accept to connect to servers
