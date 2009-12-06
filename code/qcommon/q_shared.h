8b72ea5057db9e2c024877f6a8a4e2bb66900408
sg_tequila <sg_tequila@07eb42f0-b032-4355-afd3-42b1f3832223>
2009-12-06 14:44:21 +0000

    Bump PRODUCT_VERSION & SG_RELEASE for v1.1b3 beta release
    git-svn-id: http://svn.code.sf.net/p/smokinguns/code/branches/1.1@425 07eb42f0-b032-4355-afd3-42b1f3832223

--- code/qcommon/q_shared.h
+++ code/qcommon/q_shared.h
@@ -77,10 +77,10 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
   // PRODUCT_VERSION will be used for sg_version cvar
   #ifndef PRODUCT_VERSION
-    #define PRODUCT_VERSION	1.1b2
+    #define PRODUCT_VERSION	1.1b3
   #endif
   #ifndef SG_RELEASE
-    #define SG_RELEASE	20091103
+    #define SG_RELEASE	20091206
   #endif
 
   #ifndef XSTRING
