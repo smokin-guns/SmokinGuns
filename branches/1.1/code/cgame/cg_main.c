513f84353135dac7c3d4e1ce10422671de003cd2
sg_tequila <sg_tequila@07eb42f0-b032-4355-afd3-42b1f3832223>
2009-12-21 22:31:13 +0000

    Minor cvar type fix
    git-svn-id: https://smokinguns.svn.sourceforge.net/svnroot/smokinguns@453 07eb42f0-b032-4355-afd3-42b1f3832223

--- branches/1.1/code/cgame/cg_main.c
+++ branches/1.1/code/cgame/cg_main.c
@@ -367,11 +367,13 @@ static cvarTable_t		cvarTable[] = {
 	{ &cg_runroll, "cg_runroll", "0.005", CVAR_ARCHIVE },
 #ifndef SMOKINGUNS
 	{ &cg_bobup , "cg_bobup", "0.005", CVAR_CHEAT },
-#else
-	{ &cg_bobup , "cg_bobup", "0.005", CVAR_ARCHIVE },
-#endif
 	{ &cg_bobpitch, "cg_bobpitch", "0.002", CVAR_ARCHIVE },
 	{ &cg_bobroll, "cg_bobroll", "0.002", CVAR_ARCHIVE },
+#else
+	{ &cg_bobup , "cg_bobup", "0.000", CVAR_CHEAT },
+	{ &cg_bobpitch, "cg_bobpitch", "0.000", CVAR_CHEAT },
+	{ &cg_bobroll, "cg_bobroll", "0.000", CVAR_CHEAT },
+#endif
 	{ &cg_swingSpeed, "cg_swingSpeed", "0.3", CVAR_CHEAT },
 	{ &cg_animSpeed, "cg_animspeed", "1", CVAR_CHEAT },
 	{ &cg_debugAnim, "cg_debuganim", "0", CVAR_CHEAT },
