95e8203ec1de067a7fae725fc0dde27fd91aa5b2
sparcdr <sparcdr@07eb42f0-b032-4355-afd3-42b1f3832223>
2010-01-10 19:21:28 +0000

    Added command-line build ability for Visual Studio solutions (clean.bat, build(x86|x64).bat). Added msvc2010 (vcx) support. Changed relative paths for referenced source files in msvc project solutions.
    git-svn-id: https://smokinguns.svn.sourceforge.net/svnroot/smokinguns/branches/1.1@484 07eb42f0-b032-4355-afd3-42b1f3832223

---
+++ misc/msvc/vcx/build_x86.bat
@@ -0,0 +1,28 @@
+@echo off
+echo Initializing VCX environment variables...
+call "C:\Program Files\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
+
+echo Building Debug
+msbuild sg.sln /p:configuration=Debug
+echo Building Release
+msbuild sg.sln /p:configuration=Release
+echo Building Release SSE
+msbuild sg.sln /p:configuration="Release SSE"
+echo Building Release SSE2
+msbuild sg.sln /p:configuration="Release SSE2"
+
+echo Reiterating build process for sanity.
+
+echo Building Debug
+msbuild sg.sln /p:configuration=Debug
+echo Building Release
+msbuild sg.sln /p:configuration=Release
+echo Building Release SSE
+msbuild sg.sln /p:configuration="Release SSE"
+echo Building Release SSE2
+msbuild sg.sln /p:configuration="Release SSE2"
+
+echo Build finished.
+pause
+
+IF ERRORLEVEL 1 EXIT 1
