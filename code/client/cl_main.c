d0f22406d2023e048f618083bd5180e5c1ca3b76
Thilo Schulz <arny@ats.s.bawue.de>
2011-08-02 23:34:50 +0000

    Fix game restart after curl download finished
    (cherry picked from commit 0bc54ab6963b157b81ce93f94af80de61663962c)

--- code/client/cl_main.c
+++ code/client/cl_main.c
@@ -164,7 +164,7 @@ int serverStatusCount;
 	void hA3Dg_ExportRenderGeom (refexport_t *incoming_re);
 #endif
 
-static int isQuitting = qfalse;
+static int noGameRestart = qfalse;
 
 extern void SV_BotFrame( int time );
 void CL_CheckForResend( void );
@@ -1503,8 +1503,10 @@ void CL_Disconnect( qboolean showMainMenu ) {
 
 	CL_UpdateGUID( NULL, 0 );
 
-	if(!isQuitting)
+	if(!noGameRestart)
 		CL_OldGame();
+	else
+		noGameRestart = qfalse;
 }
 
 
@@ -1800,6 +1802,7 @@ void CL_Connect_f( void ) {
 	Cvar_Set( "sv_killserver", "1" );
 	SV_Frame( 0 );
 
+	noGameRestart = qtrue;
 	CL_Disconnect( qtrue );
 	Con_Close();
 
@@ -3765,7 +3768,7 @@ void CL_Shutdown(char *finalmsg, qboolean disconnect, qboolean quit)
 	}
 	recursive = qtrue;
 
-	isQuitting = quit;
+	noGameRestart = quit;
 
 	if(disconnect)
 		CL_Disconnect(qtrue);
