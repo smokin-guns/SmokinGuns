8538ce4fed0c649b29050ff294bde53a78210a4e
joe_kari <joe_kari@07eb42f0-b032-4355-afd3-42b1f3832223>
2012-02-27 12:14:09 +0000

    merging checkins 591 into 1.1-release
    git-svn-id: http://svn.code.sf.net/p/smokinguns/code/branches/1.1-release@624 07eb42f0-b032-4355-afd3-42b1f3832223

--- code/cgame/cg_drawtools.c
+++ code/cgame/cg_drawtools.c
@@ -22,7 +22,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 ===========================================================================
 */
 //
-// cg_drawtools.c -- helper functions called by cg_draw, cg_scoreboard, cg_info, etc
+// cg_drawtools.c -- helper functions called by cg_draw, cg_info, etc
 #include "cg_local.h"
 
 /*
