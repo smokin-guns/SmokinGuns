451546aa2b81695620ec3d76341b9aafd6538b98
Zack Middleton <zturtleman@gmail.com>
2012-06-18 16:16:57 +0000

    add a missing '\n', remove some unwanted ones
    From /dev/humancontroller.
    (cherry picked from commit a4c61d874d2b5e27f8bccc9a07592133c3e11df6)

--- code/game/g_mem.c
+++ code/game/g_mem.c
@@ -47,7 +47,7 @@ void *G_Alloc( int size ) {
 	}
 
 	if ( allocPoint + size > POOLSIZE ) {
-		G_Error( "G_Alloc: failed on allocation of %i bytes\n", size );
+		G_Error( "G_Alloc: failed on allocation of %i bytes", size );
 		return NULL;
 	}
 
