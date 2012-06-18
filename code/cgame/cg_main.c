0d9f5df2d0bf715c2cd7db18bb5e3a7c3e6fc7c2
Zack Middleton <zturtleman@gmail.com>
2012-06-18 16:36:21 +0000

    Removed unneeded va() and use CG_Error instead of trap_Error in cgame. Found by Ensiform. (cherry picked from commit 2579738256d5b888e849ce82352ac51438643543)

--- code/cgame/cg_main.c
+++ code/cgame/cg_main.c
@@ -2394,7 +2394,7 @@ void CG_LoadMenus(const char *menuFile) {
 		Com_Printf( S_COLOR_YELLOW "menu file not found: %s, using default\n", menuFile );
 		len = trap_FS_FOpenFile( "ui/hud.txt", &f, FS_READ );
 		if (!f) {
-			trap_Error( va( S_COLOR_RED "default menu file not found: ui/hud.txt, unable to continue!" ) );
+			CG_Error( S_COLOR_RED "default menu file not found: ui/hud.txt, unable to continue!" );
 		}
 	}
 #else
@@ -2406,7 +2406,7 @@ void CG_LoadMenus(const char *menuFile) {
 
 	if ( len >= MAX_MENUDEFFILE ) {
 		trap_FS_FCloseFile( f );
-		trap_Error( va( S_COLOR_RED "menu file too large: %s is %i, max allowed is %i", menuFile, len, MAX_MENUDEFFILE ) );
+		CG_Error( S_COLOR_RED "menu file too large: %s is %i, max allowed is %i", menuFile, len, MAX_MENUDEFFILE );
 		return;
 	}
 
