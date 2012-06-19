4facf3f3a0ce520cb6a94be2b119e04052be254d
Zack Middleton <zturtleman@gmail.com>
2012-06-19 14:52:22 +0000

    remove a bunch of superfluous formatting calls
    From /dev/humancontroller.
    (cherry picked from commit 2db73231ebd237c0b3a6eb7b249479906a50bf02)

--- code/game/g_main.c
+++ code/game/g_main.c
@@ -960,7 +960,7 @@ void QDECL Com_Error ( int level, const char *error, ... ) {
 	Q_vsnprintf (text, sizeof(text), error, argptr);
 	va_end (argptr);
 
-	G_Error( "%s", text);
+	trap_Error( text );
 }
 
 void QDECL Com_Printf( const char *msg, ... ) {
@@ -971,7 +971,7 @@ void QDECL Com_Printf( const char *msg, ... ) {
 	Q_vsnprintf (text, sizeof(text), msg, argptr);
 	va_end (argptr);
 
-	G_Printf ("%s", text);
+	trap_Print( text );
 }
 
 
