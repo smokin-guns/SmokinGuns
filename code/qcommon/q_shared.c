8a7c6a4d5220b27a08003da85112a142aec8b8b5
Zack Middleton <zturtleman@gmail.com>
2013-06-02 21:21:45 -0500

    Fix setting COM_Parse current line number
    Fix initial off-by-one error.
    Count lines in /* */ comments and multi-line strings.
    Fix counting some lines twice if text has Unix newlines.
    
    (cherry picked from commit c0a21d08986a589f4295944ff7e5593aefc5af37)

--- code/qcommon/q_shared.c
+++ code/qcommon/q_shared.c
@@ -291,7 +291,7 @@ static	int		com_lines;
 
 void COM_BeginParseSession( const char *name )
 {
-	com_lines = 0;
+	com_lines = 1;
 	Com_sprintf(com_parsename, sizeof(com_parsename), "%s", name);
 }
 
@@ -475,6 +475,10 @@ char *COM_ParseExt( char **data_p, qboolean allowLineBreaks )
 			data += 2;
 			while ( *data && ( *data != '*' || data[1] != '/' ) )
 			{
+				if ( *data == '\n' )
+				{
+					com_lines++;
+				}
 				data++;
 			}
 			if ( *data )
@@ -501,6 +505,10 @@ char *COM_ParseExt( char **data_p, qboolean allowLineBreaks )
 				*data_p = ( char * ) data;
 				return com_token;
 			}
+			if ( c == '\n' )
+			{
+				com_lines++;
+			}
 			if (len < MAX_TOKEN_CHARS - 1)
 			{
 				com_token[len] = c;
@@ -519,8 +527,6 @@ char *COM_ParseExt( char **data_p, qboolean allowLineBreaks )
 		}
 		data++;
 		c = *data;
-		if ( c == '\n' )
-			com_lines++;
 	} while (c>32);
 
 	com_token[len] = 0;
