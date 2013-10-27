992f2bb418fa42bd79e029a1496c2936e2365d40
Zack Middleton <zturtleman@gmail.com>
2013-10-26 22:36:09 -0500

    Remove unused variable baseOffset from fsh[]
    (cherry picked from commit c69db4a4002bd640c638f7314381c5d3b3a6864a)

--- code/qcommon/files.c
+++ code/qcommon/files.c
@@ -278,7 +278,6 @@ typedef struct qfile_us {
 typedef struct {
 	qfile_ut	handleFiles;
 	qboolean	handleSync;
-	int			baseOffset;
 	int			fileSize;
 	int			zipFilePos;
 	qboolean	zipFile;
@@ -4047,11 +4046,6 @@ int		FS_FOpenFileByMode( const char *qpath, fileHandle_t *f, fsMode_t mode ) {
 	}
 
 	if ( *f ) {
-		if (fsh[*f].zipFile == qtrue) {
-			fsh[*f].baseOffset = unztell(fsh[*f].handleFiles.file.z);
-		} else {
-			fsh[*f].baseOffset = ftell(fsh[*f].handleFiles.file.o);
-		}
 		fsh[*f].fileSize = r;
 		fsh[*f].streamed = qfalse;
 
